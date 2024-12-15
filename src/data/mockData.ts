db9cc5e7b297c3abf5c581bd89fb2c711132e931
anjana732 <anjanasingh1257@gmail.com>
2024-12-15 16:41:06 +0530

    Add User

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,4 +1,4 @@
 export const mockInterns = [
-  { id: '1', name: 'John Doe', email: 'john@example.com' },
-  { id: '2', name: 'Jane Smith', email: 'jane@example.com' },
+  { id: '1', name: 'Anjana', email: 'john@example.com' },
+  { id: '2', name: 'Manu', email: 'jane@example.com' },
 ] as const;
